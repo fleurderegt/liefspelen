282c69b1020b4cda1b620123a36069ce3537e099
Fleur de Regt <fleurderegt@gmail.com>
2017-08-19 12:15:18 +0200

    change url image on detail

--- liefspelen/forms.py
+++ liefspelen/forms.py
@@ -8,4 +8,8 @@ class GameForm(forms.ModelForm):
         model = Game
         fields = ('title', 'text','categories','tags','seasons','environments','min_age','max_age','min_players','max_players', 'materials', 'image')
 
-     
+class SearchForm(forms.ModelForm):
+
+    class Meta:
+        model = Game
+        fields = ('title', 'text','categories','tags','seasons','environments','min_age','max_age','min_players','max_players', 'materials', 'image')  
